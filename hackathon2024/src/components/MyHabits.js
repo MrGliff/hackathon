bf86abc2151c8bf7d116d599445715c58b5f9140
Yatharth Singh Panwar <130735103+yatharth-singh-panwar@users.noreply.github.com>
2024-03-15 07:40:20 +0530

    Add files via upload
    My habits made

---
+++ hackathon2024/src/components/MyHabits.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Divisions from "./Divisions.js"
+export default function  MyHabits(){
+    return(
+    <>
+        <Divisions Name="Cycling" Hours="42" Avg="2" Days="8"/>
+        <Divisions Name="Reading" Hours="60" Avg="3" Days="18"/>
+        <Divisions Name="Gymming" Hours="64" Avg="8" Days="20"/>
+    </>
+    )
+    
+}
